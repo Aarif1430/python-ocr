9d654948a1f697bbc1606ff174fe6e6768786d64
Aarif1430 <malikarif13@gmail.com>
2020-08-05 20:29:47 +0530

    optimize 8

--- app/routes.py
+++ app/routes.py
@@ -16,19 +16,20 @@ def index():
     return render_template('index.html', the_title='Character Recognition using CNN')
 
 
-def naiveInterp2D(M, newx, newy):
-    result = np.zeros((newx,newy))
-    for i in range(M.shape[0]):
-        for j in range(M.shape[1]):
-            indx = int(i*newx / M.shape[0])
-            indy = int(j*newy / M.shape[1])
-            result[indx,indy] +=M[i,j]
+def resize_with_interpolation(img, newx, newy):
+    result = np.zeros((newx, newy))
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            index_x = int(i*newx / img.shape[0])
+            index_y = int(j*newy / img.shape[1])
+            result[index_x, index_y] += img[i, j]
     return result
+
 
 @app.route('/get_canvas_data', methods=['GET', 'POST'])
 def process_image_data():
-    jpgtxt = request.args.get('img', 0, type=str)
-    data = jpgtxt.split(',')[-1]
+    data_uri = request.args.get('img', 0, type=str)
+    data = data_uri.split(',')[-1]
     data = base64.b64decode(data.encode('ascii'))
 
     g = open("temp.jpg", "wb")
@@ -37,7 +38,7 @@ def process_image_data():
     img = Image.open('temp.jpg').convert('RGBA')
     im = np.array(img)
     im = im.reshape(im.shape[0], (im.shape[1]*im.shape[2]))
-    resized = np.asarray(255.0 - (np.float32(naiveInterp2D(im,20,20)).reshape(20, 20) * 255))
+    resized = np.asarray(255.0 - (np.float32(resize_with_interpolation(im, 20, 20)).reshape(20, 20) * 255))
     fn = lambda x: 0 if x < 200 else 255
     vfunc = np.vectorize(fn)
     grayed = vfunc(resized)
